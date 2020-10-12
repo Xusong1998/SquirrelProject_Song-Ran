16d9744135f04903084c795579c1e39cee50a103
Song Xu <sx2284@columbia.edu>
2020-10-12 01:03:07 +0000

    Changing url file

--- project/urls.py
+++ project/urls.py
@@ -2,4 +2,7 @@ from django.urls import path, include
 from django.contrib import admin
 from . import views
 
-url_patterns = [path('admin/', admin.site.urls]
+urlpatterns = [
+        path('', views.index),]
+
+
