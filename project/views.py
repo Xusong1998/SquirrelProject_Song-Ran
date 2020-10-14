df68ace62f610d492a3e04aeeaf44ac99041d56c
Song Xu <sx2284@columbia.edu>
2020-10-14 23:58:17 +0000

    /map page added successfully, please move on to other pages.

--- project/views.py
+++ project/views.py
@@ -6,7 +6,7 @@ def index(request):
     return render(request, 'project/home_page.html',{})
 
 def map(request):
-    squirrels = Squirrel.objects.all()
+    squirrels = Squirrel.objects.all()[:100]
     context = {
             'sightings': squirrels,
             }
